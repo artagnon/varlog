68df6410d64a2eeeab948f17404787ca2786cae2
Ramkumar Ramachandra <artagnon@gmail.com>
2013-03-05 16:28:18 +0530

    views/user.slim, web.rb: make /:username render

--- web.rb
+++ web.rb
@@ -19,6 +19,16 @@ get '/' do
   slim :index
 end
 
+get '/:username' do
+  @username = params[:username]
+  @streamentries = []
+  settings.mongo_db[@username].find.to_a.each do |entry|
+    @streamentries << Struct.new(:title, :album, :artist)
+      .new(entry["title"], entry["album"], entry["artist"])
+  end
+  slim :user
+end
+
 get '/:username/stream' do
   content_type :json
   settings.mongo_db[params[:username]].find.to_a.to_json
