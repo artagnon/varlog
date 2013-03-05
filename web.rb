11892ec4557562063a491f75fb916db869bedf16
Ramkumar Ramachandra <artagnon@gmail.com>
2013-03-05 16:20:43 +0530

    web.rb: configure Mongo db connection

--- web.rb
+++ web.rb
@@ -1,6 +1,19 @@
 require 'sinatra'
 require 'slim'
 require 'json'
+require 'mongo'
+
+include Mongo
+
+configure do
+  if ENV['MONGOHQ_URL']
+    ENV['MONGODB_URI'] = ENV['MONGOHQ_URL']
+    dbstr = ENV['MONGOHQ_URL'].split('/')[-1]
+    set :mongo_db, MongoClient.new.db(dbstr)
+  else
+    set :mongo_db, MongoClient.new.db('local')
+  end
+end
 
 get '/' do
   slim :index
